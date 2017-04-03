08784ac21b8aff5575e977894f4ed9ec8a24e5e5
Di Pietro Adrian <adrian.dipietro@heig-vd.ch>
2017-04-03 16:14:36 +0200

    BounceApp completed

--- src/bounce/BounceApp.java
+++ src/bounce/BounceApp.java
@@ -5,12 +5,18 @@ import java.util.LinkedList;
 public class BounceApp {
 
     private LinkedList<Bouncable> bouncers;
-
+    private Displayer dm;
     public BounceApp() {
-
+        dm = DisplayManager.getInstance();
     }
     public void loop() {
-
+        while (true) {
+            for (Bouncable b : bouncers) {
+                b.move();
+                b.draw();
+            }
+            dm.repaint();
+        }
     }
     public static void main(String ... args) {
         new BounceApp().loop();
